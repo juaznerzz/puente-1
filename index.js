c5f6d6eaea5b60cded20e8875f2a88458c7944d4
juaznerzz <juaznerzzcop@gmail.com>
2025-06-16 16:11:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-const express = require ( 'express' );
-constante cors = require ( 'cors' );
-const aplicación = express ();
-const puerto =
- proceso.env.PORT || 3000 ;​
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Configuración de CORS para permitir solicitudes de cualquier origen
-aplicación. uso ( cors ());
+app.use(cors());
 
 // Middleware para manejar JSON
-aplicación.use (express.json ( ))
- ;
+app.use(express.json());
 
 // Ruta para validar la clave de licencia
-aplicación.post ( '/api/ v1 /validate' , ( req, res ) => {
+app.post('/api/v1/validate', (req, res) => {
     // Recibir los datos de la solicitud
-    const { sub_clave, id_único, mo_no, slug, b_version, r_id } = cuerpo requerido ;
-    // slug, b_version y r_id se reciben del cliente pero no se
-    // validados en el servidor. Están reservados para uso futuro y actualmente...
-    // ignorado en la lógica de validación.
+    const { sub_key, unique_id, mo_no, slug, b_version, r_id } = req.body;
+    // slug, b_version and r_id are received from the client but are not
+    // validated on the server. They are reserved for future use and currently
+    // ignored in the validation logic.
 
     // Verificar si la clave de licencia y el ID de usuario son correctos
-    if (sub_key === "123456" && Unique_id === "KH6kRZbbMwGZNpP7BZfubCG7sBhx9yJBYYAauyuN5EXKCHP" && mo_no === "593961758817" ) {
+    if (sub_key === "123456" && unique_id === "KH6kRZbbMwGZNpP7BZfubCG7sBhx9yJBYYAauyuN5EXKCHP" && mo_no === "593961758817") {
         // Si es válido, devolver un estado 200 y el mensaje de éxito
-        devolver res. estado ( 200 ) .json ({
-            válido : verdadero ,
-            Mensaje : 'Licencia validada exitosamente'
+        return res.status(200).json({
+            valid: true,
+            message: 'License validated successfully'
         });
     }
 
     // Si no es válido, devolver un error 400 y el mensaje de error
-    devolver res. estado ( 400 ) .json ({
-        válido : falso ,
-        Mensaje : 'Licencia o ID de usuario no válidos'
+    return res.status(400).json({
+        valid: false,
+        message: 'Invalid license or user ID'
     });
 });
 
 // Iniciar el servidor
-aplicación. escuchar (puerto, () => {
-    console.log ( `Servidor ejecutándose en el puerto $ {port} ` ) ;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
